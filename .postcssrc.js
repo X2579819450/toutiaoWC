0a98f625cf74fe14d3a4cfbce20fae53c2b19987
SyrianSoldier <9975545+syriansoldier@user.noreply.gitee.com>
2022-09-06 18:12:25 +0800

    feat:登录页完成布局

--- .postcssrc.js
+++ .postcssrc.js
@@ -7,7 +7,7 @@ module.exports = {
       // 根节点字体大小
       // 如果是vant 37.5 如果自己的代码 75
       // file当前编译的css的文件路径
-      rootValue: ({ file }) => (/vant/.test(file) ? 37.5 : 75),
+      rootValue: (module) => (/vant/.test(module.file) ? 37.5 : 75),
       propList: ['*'] // 所有的属性都转成rem
     })
   ]
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,10 @@
 <template>
   <div>
-    <span class="toutiao toutiao-gengduo"></span>
-
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-
-    <div class="login">登录</div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-// rem适配
-
-// 1. 让rem动起来
-//  - yarn add amfe-flexible
-// 2. 盒子使用rem
-//  - 自动的识别css中px, 并将其转成rem.
-
-// post-css css的预处理器
-// - postcss 自己能干的事很少, 下载插件
-
-// 1. 下载 yarn add postcss-pxtorem
-// 2. 告诉postcss, 我要使用这个插件, .postcssrc.js
 export default {}
 </script>
 
