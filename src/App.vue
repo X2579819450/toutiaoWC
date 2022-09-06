a3bd436e91cbda20a8e49bb691aff68014e02f5a
SyrianSoldier <9975545+syriansoldier@user.noreply.gitee.com>
2022-09-06 16:12:36 +0800

    feat:postcss配置完毕

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <template>
   <div>
     <span class="toutiao toutiao-gengduo"></span>
+
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <div class="login">登录</div>
   </div>
 </template>
 
 <script>
-// git log 查看commit记录
-// git status 查看当前仓库的状态
-// git add . 提交到暂存区
-// git commit -m ''
-//  - feat: 新增特性
-//  - style: 样式的修改
-//  - fixed: debug
-//  - comment: 添加了注释
+// rem适配
+
+// 1. 让rem动起来
+//  - yarn add amfe-flexible
+// 2. 盒子使用rem
+//  - 自动的识别css中px, 并将其转成rem.
+
+// post-css css的预处理器
+// - postcss 自己能干的事很少, 下载插件
+
+// 1. 下载 yarn add postcss-pxtorem
+// 2. 告诉postcss, 我要使用这个插件, .postcssrc.js
 export default {}
 </script>
 
-<style></style>
+<style scoped>
+.login {
+  width: 700px;
+  height: 88px;
+  background: skyblue;
+}
+</style>
