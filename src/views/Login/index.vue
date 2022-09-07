d42b0a1f63b1abc5a646f40943b6da247c68d300
SyrianSoldier <9975545+syriansoldier@user.noreply.gitee.com>
2022-09-07 17:17:27 +0800

    feat:登录页完成

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -9,19 +9,12 @@
     <!-- name:表示表单项 -->
     <!-- rules是一个数组 -->
     <!-- rule:{ required:是否必选,message:错误的提示信息,pattern:正则,trigger:规则的触发时机onChange、onBlur } -->
-    <van-form @submit="onSubmit" class="form">
+    <van-form @submit="onSubmit" class="form" ref="form">
       <van-field
         v-model="mobile"
         name="mobile"
         placeholder="请输入手机号"
-        :rules="[
-          { required: true, message: '请填写手机号' },
-          {
-            pattern:
-              /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/,
-            message: '手机格式错误'
-          }
-        ]"
+        :rules="mobileRules"
       >
         <template #label>
           <span class="toutiao toutiao-shouji"></span>
@@ -32,15 +25,35 @@
         v-model="code"
         name="code"
         placeholder="请输入验证码"
-        :rules="[
-          { required: true, message: '请输入验证码' },
-          { pattern: /[0-9]{6}/, message: '验证码格式错误' }
-        ]"
+        :rules="codeRules"
       >
+        <!-- 字体图标 -->
         <template #label>
           <span class="toutiao toutiao-yanzhengma"></span>
         </template>
+        <!-- 验证码 -->
+        <template #button>
+          <van-button
+            class="btn"
+            block
+            type="default"
+            size="small"
+            round
+            native-type="button"
+            v-if="isShowCodeBtn"
+            @click="sendCode"
+            >获取验证码</van-button
+          >
+
+          <van-count-down
+            v-else
+            :time="3 * 1000"
+            format="ss秒"
+            @finish="isShowCodeBtn = true"
+          />
+        </template>
       </van-field>
+
       <div style="margin: 16px">
         <van-button block type="info" native-type="submit">登录</van-button>
       </div>
@@ -49,16 +62,95 @@
 </template>
 
 <script>
+import { mobileRules, codeRules } from './rule'
+// 引入API
+import { login, sendCodeAPI } from '@/api'
+import { mapMutations } from 'vuex'
+// token 身份证 标识用户
+//  用处: 标识用户是否登陆了
+// token 存放vuex
+//  - toekn 存在 vuex
 export default {
   data() {
     return {
       mobile: '',
-      code: ''
+      code: '',
+      mobileRules,
+      codeRules,
+      isShowCodeBtn: true
     }
   },
   methods: {
-    onSubmit(values) {
-      console.log('submit', values)
+    ...mapMutations(['SET_TOKEN']),
+    async onSubmit() {
+      // submit事件只有表单校验通过以后会被触发
+
+      // loading
+      // message: 提示文案
+      // forbidClick 禁止点击
+      // duration 展示的时长. 为0, 一直展示
+      this.loading()
+
+      try {
+        // 登录
+        const { data } = await login(this.mobile, this.code)
+        // 将token存进vuex
+        this.SET_TOKEN(data.data)
+        // 跳转路由
+        this.$router.push('/profile')
+        // 成功的提示
+        this.$toast.success('登录成功')
+      } catch (error) {
+        // 细分一下失败: 提示用户手机号和验证码
+        // 如果是手机号或者验证码错了, 用户能知道
+        // error: 1. js抛的错 2.axios封装的error对象
+
+        // axios封装的error对象
+        //  - error.response.data  后端返回的数据
+        //  - error.response.status 后端返回的状态码
+        if (error.response && error.response.status === 400) {
+          this.$toast.fail(error.response.data.message)
+        } else {
+          // 1. js导致错误, 2. 507
+          // console.dir(error)
+          this.$toast.clear()
+          throw error
+        }
+      }
+    },
+    loading() {
+      this.$toast.loading({
+        message: '加载中...',
+        forbidClick: true,
+        duration: 0
+      })
+    },
+    async sendCode() {
+      // 0. 验证用户是否输入了有效的手机号
+      //  - 1 form绑定ref
+      //  - 2. $refs.form.validate(name)
+      await this.$refs.form.validate('mobile')
+
+      this.loading()
+      // 1.发送请求
+      try {
+        await sendCodeAPI(this.mobile)
+
+        this.isShowCodeBtn = false
+
+        this.$toast.success('发送验证码成功')
+      } catch (error) {
+        if (
+          error.response &&
+          (error.response.status === 429 || error.response.status === 404)
+        ) {
+          // axios的错误
+          this.$toast.fail(error.response.data.message)
+        } else {
+          this.$toast.clear()
+          throw error
+        }
+      }
     }
   }
 }
@@ -86,5 +178,11 @@ export default {
   .toutiao {
     font-size: 40px;
   }
+}
+
+.btn {
+  height: 0.64rem;
+  background-color: #eee;
+  color: #a58594;
 }
 </style>
---
+++ src/vuex简介.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <!-- <router-view></router-view> -->
+    <!-- 展示count -->
+    <h1>{{ count }}</h1>
+    <h1>count的十倍: {{ bigNum }}</h1>
+    <button @click="SET_COUNT(10)">点击count</button>
+  </div>
+</template>
+
+<script>
+// mapState --> 将vuex里面state映射到computed
+// 使用
+//  - 引入mapState: 函数, 返回一个对象
+//  - 参数: ['映射的属性']
+
+// mapGetters --> 将vuex里面getters映射到computed
+
+// mapMutations --> 将vuex的mutations方法映射到methods
+import { mapState, mapGetters, mapMutations } from 'vuex'
+
+export default {
+  methods: {
+    // increment() {
+    //   this.$store.commit('SET_COUNT', 10)
+    // }
+    ...mapMutations(['SET_COUNT'])
+  },
+  computed: {
+    ...mapState(['count']),
+    ...mapGetters(['bigNum'])
+  }
+}
+</script>
+
+<style scoped>
+.login {
+  width: 700px;
+  height: 88px;
+  background: skyblue;
+}
+</style>
